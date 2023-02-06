25d019c2abf95aec48850abb5543dab00ffceb2b
Adriana-Drika <nmdrika@gmail.com>
2023-02-06 18:05:51 -0300

    Projeto com banco de dados mais Login

--- src/pages/Admin/index.js
+++ src/pages/Admin/index.js
@@ -1,8 +1,8 @@
-import { useState, useEffect  } from 'react'
+import { useState, useEffect } from 'react'
 import './admin.css'
 
-import {auth, db} from '../../firebaseConnection'
-import {signOut} from 'firebase/auth'
+import { auth, db } from '../../firebaseConnection'
+import { signOut } from 'firebase/auth'
 
 import {
   addDoc,
@@ -16,27 +16,24 @@ import {
   updateDoc
 
 } from 'firebase/firestore'
-import { async } from '@firebase/util'
 
-export default function Admin(){
+export default function Admin() {
   const [tarefaInput, setTarefaInput] = useState('')
   const [user, setUser] = useState({})
   const [edit, setEdit] = useState({})
   const [tarefas, setTarefas] = useState([])
 
-
-
   useEffect(() => {
-    async function loadTarefas(){
+    async function loadTarefas() {
       const userDetail = localStorage.getItem('@detailUser')
-      setUser(JSON.parse(userDetail)) 
+      setUser(JSON.parse(userDetail))
 
-      if(userDetail){
+      if (userDetail) {
         const data = JSON.parse(userDetail);
-
         const tarefaRef = collection(db, 'tarefas')
         const q = query(tarefaRef, orderBy('created', 'desc'), where('userUid', '==', data?.uid))// se userUid é igual a este usuário que está logado
-        const unsub = onSnapshot(q, (snapshot) => {
+
+        onSnapshot(q, (snapshot) => {
           let lista = []
 
           snapshot.forEach((doc) => {
@@ -58,97 +55,97 @@ export default function Admin(){
     loadTarefas();
   }, [])
 
-  async function handleRegister(e){
+  async function handleRegister(e) {
     e.preventDefault();
 
-    if(tarefaInput === ''){
+    if (tarefaInput === '') {
       alert('Digite sua tarefa...')
       return;
     }
 
-    if(edit?.id){
+    if (edit?.id) {
       handleUpdateTarefa();
       return;
     }
 
     await addDoc(collection(db, 'tarefas'), {
-       tarefa: tarefaInput,
-       created: new Date(),
-       userUid: user?.uid
+      tarefa: tarefaInput,
+      created: new Date(),
+      userUid: user?.uid
     })
-    .then(() => {
-      console.log('TAREFA REGISTRADA')
-      setTarefaInput('')
-    })
-    .catch((error) => {
-      console.log('ERRO AO REGISTRAR' + error)
+      .then(() => {
+        console.log('TAREFA REGISTRADA')
+        setTarefaInput('')
+      })
+      .catch((error) => {
+        console.log('ERRO AO REGISTRAR' + error)
 
-    })
+      })
 
   }
 
-  async function handleLogout(){
+  async function handleLogout() {
     await signOut(auth);
   }
 
-  async function deleteTarefa(id){
+  async function deleteTarefa(id) {
     const docRef = doc(db, 'tarefas', id)
     await deleteDoc(docRef)
   }
 
-  function editTarefa(item){
+  function editTarefa(item) {
     setTarefaInput(item.tarefa);
     setEdit(item);
   }
 
-  async function handleUpdateTarefa(){
+  async function handleUpdateTarefa() {
     const docRef = doc(db, 'tarefas', edit?.id)
     await updateDoc(docRef, {
       tarefa: tarefaInput
     })
-    .then(() => {
-      console.log('TAREFA ATUALIZADA')
-      setTarefaInput('')
-      setEdit({})
-    })
-    .catch(() => {
-      console.log('ERRO AO ATUALIZAR')
-      setTarefaInput('')
-      setEdit({})
-    })
+      .then(() => {
+        console.log('TAREFA ATUALIZADA')
+        setTarefaInput('')
+        setEdit({})
+      })
+      .catch(() => {
+        console.log('ERRO AO ATUALIZAR')
+        setTarefaInput('')
+        setEdit({})
+      })
   }
 
-  return(
+  return (
     <div className='admin-container'>
-       <h1>Minhas tarefas </h1>
+      <h1>Minhas tarefas </h1>
 
-       <form  className ='form' onSubmit={handleRegister}>
-          <textarea
-             placeholder='Digite sua tarefa...'
-             value={tarefaInput}
-             onChange={(e) => setTarefaInput(e.target.value)}
-          />
+      <form className='form' onSubmit={handleRegister}>
+        <textarea
+          placeholder='Digite sua tarefa...'
+          value={tarefaInput}
+          onChange={(e) => setTarefaInput(e.target.value)}
+        />
 
-          {Object.keys(edit).length > 0 ? (
-            <button className='btn-register' style={{backgroundColor: '#6add39'}} type='submit'>Atualizar tarefa</button>
-          ) : (
-            <button className='btn-register' type='submit'>Registrar tarefas</button>
-          )}
-       </form>
+        {edit?.id ? (
+          <button className='btn-register' style={{ backgroundColor: '#6add39' }} type='submit'>Atualizar tarefa</button>
+        ) : (
+          <button className='btn-register' type='submit'>Registrar tarefas</button>
+        )}
+      </form>
 
-       {tarefas.map((item) => (
-       <article  key={item.id} className='list'>
+      {tarefas.map((item) => (
+        <article key={item.id} className='list'>
           <p>{item.tarefa}</p>
 
           <div>
-              <button onClick={() => editTarefa(item)}>Editar</button>
-              <button onClick={() => deleteTarefa(item.id)} className="btn-delete">Concluir</button>
+            <button onClick={() => editTarefa(item)}>Editar</button>
+            <button onClick={() => deleteTarefa(item.id)} className="btn-delete">Concluir</button>
           </div>
-       </article>
-       ))}
+        </article>
+      ))}
 
 
-       <button className='btn-logout' onClick={handleLogout}>Sair</button>
+      <button className='btn-logout' onClick={handleLogout}>Sair</button>
 
     </div>
   )
